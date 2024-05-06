a1129eb52803df9854af0c60cef50cb007e52372
Techlab <flutterhub.bd@gmail.com>
2024-05-05 21:58:11 -0700

    third one

---
+++ lib/newfolder/second/secondfile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SEcond extends StatefulWidget {
+  const SEcond({super.key});
+
+  @override
+  State<SEcond> createState() => _SEcondState();
+}
+
+class _SEcondState extends State<SEcond> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
