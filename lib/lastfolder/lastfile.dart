995f802a7d97854c52df8ee23102a8ac9aaff1a9
Techlab <flutterhub.bd@gmail.com>
2024-05-05 22:00:26 -0700

    last file

---
+++ lib/lastfolder/lastfile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lastfile extends StatefulWidget {
+  const Lastfile({super.key});
+
+  @override
+  State<Lastfile> createState() => _LastfileState();
+}
+
+class _LastfileState extends State<Lastfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
